d28f30db79fda22b6dea53dec04571df1d6348cd
aref.kanaanpoor <aref.kanaanpoor@gmail.com>
2024-09-22 15:35:49 +0330

    Add First Or Default Benchmark Project

---
+++ CSharp/FirstOrDefault/Benchmark.cs
@@ -0,0 +1,34 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace FirstOrDefault;
+
+
+[MemoryDiagnoser(false)]
+public class Benchmark
+{
+    private readonly List<int> _rowNumbers;
+
+    public Benchmark()
+    {
+        _rowNumbers = Enumerable.Range(1, 1000).ToList();
+    }
+
+    [Benchmark]
+    public int FindRow()
+    {
+        return _rowNumbers.Find(z => z == 500)!;
+    }
+
+    [Benchmark]
+    public int FirstOrDefaultRow()
+    {
+        return _rowNumbers.FirstOrDefault(z => z == 500)!;
+    }
+    
+    [Benchmark]
+    public int SingleOrDefaultRow()
+    {
+        return _rowNumbers.SingleOrDefault(z => z == 500)!;
+    }
+    
+}
---
+++ CSharp/FirstOrDefault/Program.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Validators;
+using FirstOrDefault;
+
+List<int> list = [1, 2, 3, 4, 5];
+
+var a = list.Find(z => z == 3);
+
+var b = list.FirstOrDefault(z => z == 3);
+
+#region BenckMark Rows
+
+var config = new ManualConfig()
+    .WithOptions(ConfigOptions.DisableOptimizationsValidator)
+    .AddValidator(JitOptimizationsValidator.DontFailOnError)
+    .AddLogger(ConsoleLogger.Default)
+    .AddColumnProvider(DefaultColumnProviders.Instance);
+
+BenchmarkRunner.Run<Benchmark>(config);
+
+#endregion
